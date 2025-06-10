2c4f114d0f98fe8e29c704e6f9c3721c063b92da
somy123 <dikshamishra4444@gmail.com>
2025-06-11 01:55:32 +0530

    apicall

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useRef ,forwardRef} from "react";
-const MyButton = forwardRef( (props,ref) => {
-  console.log(props);
-  return <input  ref={ref} placeholder="search..." />;
-});
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  const inputRef = useRef();
-  const onFocusClick = () => {
-    inputRef.current.focus();
+  const [count, setCount] = useState(1);
+  const [products, setProducts] = useState([]);
+  const callcount = () => {
+    setCount(count + 1);
+  };
+  useEffect(() => {
+    console.log("hello");
+    const url = "https://dummyjson.com/products";
+
+    getData(url);
+  }, [count]);
+
+  const getData = async (url) => {
+    try {
+      const {
+        data: { products },
+      } = await axios.get(url);
+      setProducts(products);
+      console.log(products);
+    } catch (err) {
+      return err;
+    }
   };
 
   return (
-    <div>
-      <MyButton placeholder="search...." ref={inputRef} />
-      <button onClick={onFocusClick}>Focus on Input</button>
-    </div>
+    <>
+      <button onClick={callcount}>increase</button>
+      <p>{count}</p>
+      {products &&
+        products.length > 0 &&
+        products.map((prod) => <p key={prod.id}>{prod.title}</p>)}
+    </>
   );
 }
 
