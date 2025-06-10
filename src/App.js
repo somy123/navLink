d3c8fed614379f22fbc2ec207a437541f790124c
somy123 <dikshamishra4444@gmail.com>
2025-06-10 20:23:21 +0530

    useref demo

--- src/App.js
+++ src/App.js
@@ -1,58 +1,30 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { v4 as uuid } from "uuid";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function App() {
-  const [todo, setTodo] = useState("");
-  const [todoList, setTodoList] = useState([]);
-  const addTodoClick = () => {
-    setTodoList([...todoList, { id: uuid(), todo: todo, ischecked: false }]);
-    setTodo("");
+  const timeref = useRef("somya");
+  const [count, setCount] = useState(0);
+  //let timer = "";
+  console.log("before settimeout", timeref.current);
+  const buttonClicked = () => {
+    console.log(timeref.current);
+    timeref.current = setInterval(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+    console.log("after setTime out", timeref.current);
   };
-  const onTodoInputChange = (e) => {
-    setTodo(e.target.value);
-  };
-  const deleteClicked = (id) => {
-    const updatedtodoList = todoList.filter((todo) => todo.id !== id);
-    setTodoList(updatedtodoList);
-    console.log(id, todoList);
-  };
-  const todoCheck = (id) => {
-    const updatedtodoList = todoList.map((todo) =>
-      todo.id === id ? { ...todo, ischecked: !todo.ischecked } : todo
-    );
-    setTodoList(updatedtodoList);
-    console.log(todoList);
+  const stopClicked = () => {
+    console.log("last", timeref.current);
+    clearInterval(timeref.current);
   };
 
-  console.log(todoList);
   return (
     <div className="app">
-      <h1>My Wishlist</h1>
-      <div>
-        <input
-          value={todo}
-          onChange={onTodoInputChange}
-          placeholder="Add your wishlist here..."
-        />
-        <button onClick={() => addTodoClick(todo.id)}>Add</button>
-      </div>
-      <div>
-        {todoList &&
-          todoList.length > 0 &&
-          todoList.map((todo) => {
-            return (
-              <div className={todo.ischecked ? "myclass" : ""} key={todo.id}>
-                <label>
-                  <input onChange={() => todoCheck(todo.id)} type="checkbox" />
-                  <span>{todo.todo}</span>
-                </label>
-                <button onClick={() => deleteClicked(todo.id)}>delete</button>
-              </div>
-            );
-          })}
-      </div>
+      <p>{count}</p>
+      <button onClick={buttonClicked}>count</button>
+      <button onClick={stopClicked}>stop</button>
     </div>
   );
 }
