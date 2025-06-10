0a02f2ee6df156ee80149e7f5ad1c6c719964333
somy123 <dikshamishra4444@gmail.com>
2025-06-11 02:31:24 +0530

    cleanup timeinterval

--- src/App.js
+++ src/App.js
@@ -1,40 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState, useEffect } from "react";
-import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(1);
-  const [products, setProducts] = useState([]);
-  const callcount = () => {
-    setCount(count + 1);
-  };
+  const [count, counter] = useState(0);
   useEffect(() => {
-    console.log("hello");
-    const url = "https://dummyjson.com/products";
+    console.log("useeffect")
+    const clearid=setInterval(() => {
+      counter((count) => count + 1);
+    }, 1000);
 
-    getData(url);
-  }, [count]);
-
-  const getData = async (url) => {
-    try {
-      const {
-        data: { products },
-      } = await axios.get(url);
-      setProducts(products);
-      console.log(products);
-    } catch (err) {
-      return err;
+    return ()=>{
+      console.log("cleanup function");
+      clearInterval(clearid);
     }
-  };
-
+  }, []);
   return (
     <>
-      <button onClick={callcount}>increase</button>
       <p>{count}</p>
-      {products &&
-        products.length > 0 &&
-        products.map((prod) => <p key={prod.id}>{prod.title}</p>)}
     </>
   );
 }
