96453e6405fa9818724b1d32f25236a7bcc2f782
somy123 <dikshamishra4444@gmail.com>
2025-06-11 00:36:53 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { v4 as uuid } from "uuid";
-import { useRef, useState } from "react";
+import { useRef ,forwardRef} from "react";
+const MyButton = forwardRef( (props,ref) => {
+  console.log(props);
+  return <input  ref={ref} placeholder="search..." />;
+});
 
 function App() {
-  const timeref = useRef("somya");
-  const [count, setCount] = useState(0);
-  //let timer = "";
-  console.log("before settimeout", timeref.current);
-  const buttonClicked = () => {
-    console.log(timeref.current);
-    timeref.current = setInterval(() => {
-      setCount((count) => count + 1);
-    }, 1000);
-    console.log("after setTime out", timeref.current);
-  };
-  const stopClicked = () => {
-    console.log("last", timeref.current);
-    clearInterval(timeref.current);
+  const inputRef = useRef();
+  const onFocusClick = () => {
+    inputRef.current.focus();
   };
 
   return (
-    <div className="app">
-      <p>{count}</p>
-      <button onClick={buttonClicked}>count</button>
-      <button onClick={stopClicked}>stop</button>
+    <div>
+      <MyButton placeholder="search...." ref={inputRef} />
+      <button onClick={onFocusClick}>Focus on Input</button>
     </div>
   );
 }
