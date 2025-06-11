c33667b1e5a01af3939d762806f3895fc8e347d5
somy123 <dikshamishra4444@gmail.com>
2025-06-11 18:06:47 +0530

    objectasdependencyarray

--- src/App.js
+++ src/App.js
@@ -3,22 +3,39 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [count, counter] = useState(0);
+  const addresses = [
+    { city: "mumbai", pincode: "400029", isDefault: true },
+    { city: "Delhi", pincode: "400029", isDefault: false },
+    { city: "Chennai", pincode: "400090", isDefault: false },
+    { city: "Kolkata", pincode: "400095", isDefault: false },
+  ];
+  const [defaultAddress, setDefaultAddress] = useState(
+    addresses.find((item) => item.isDefault === true)
+  );
+  const [pincode, setPinCode] = useState("");
+  const onSetPinCodeClick = () => {
+    const selectedAddress = addresses.find(
+      (address) => address.pincode === pincode
+    );
+    setDefaultAddress(selectedAddress);
+  };
   useEffect(() => {
-    console.log("useeffect")
-    const clearid=setInterval(() => {
-      counter((count) => count + 1);
-    }, 1000);
-
-    return ()=>{
-      console.log("cleanup function");
-      clearInterval(clearid);
-    }
-  }, []);
+    console.log("useeffect called");
+  }, [defaultAddress.pincode]);
+  console.log(defaultAddress);
   return (
-    <>
-      <p>{count}</p>
-    </>
+    <div className="app">
+      <h2>Dependancy Mistakes in useeffect hook</h2>
+      <div>
+        <input
+          onChange={(e) => setPinCode(e.target.value)}
+          placeholder="enter pincode"
+        />
+      </div>
+      <div>
+        <button onClick={onSetPinCodeClick}>set pincode</button>
+      </div>
+    </div>
   );
 }
 
